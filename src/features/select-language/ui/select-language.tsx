daa23c7cde4fda798586c6030baa4cf18b81462f
nert1n <dymufa@gmail.com>
2025-03-23 14:24:48 +0200

    feat: updated select language popover

--- src/features/select-language/ui/select-language.tsx
+++ src/features/select-language/ui/select-language.tsx
@@ -1,4 +1,5 @@
-import { ChangeEvent, useEffect, useState } from "react";
+import { clsx } from "clsx";
+import { useEffect, useState, useRef } from "react";
 import { useTranslation } from "react-i18next";
 
 import { LANGUAGES } from "@shared/consts/languages";
@@ -7,12 +8,19 @@ import styles from "./select-language.module.scss";
 import { ISelectLang } from "../model/types";
 
 export const SelectLanguage = ({ className }: ISelectLang) => {
-	const { i18n, t } = useTranslation();
+	const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+	const { i18n } = useTranslation();
 	const [lang, setLang] = useState("en");
+	const popoverRef = useRef<HTMLDivElement | null>(null);
 
-	const languageChange = (e: ChangeEvent<HTMLSelectElement>) => {
-		setLang(e.target.value);
-		i18n.changeLanguage(e.target.value);
+	const languageChange = (lang: string) => {
+		setLang(lang);
+		i18n.changeLanguage(lang);
+		setIsPopoverOpen(false);
+	};
+
+	const togglePopover = () => {
+		setIsPopoverOpen(prev => !prev);
 	};
 
 	useEffect(() => {
@@ -20,21 +28,42 @@ export const SelectLanguage = ({ className }: ISelectLang) => {
 		document.documentElement.lang = i18n.language;
 	}, [i18n]);
 
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				popoverRef.current &&
+				!popoverRef.current.contains(event.target as Node)
+			) {
+				setIsPopoverOpen(false);
+			}
+		};
+
+		if (isPopoverOpen) {
+			document.addEventListener("mousedown", handleClickOutside);
+		} else {
+			document.removeEventListener("mousedown", handleClickOutside);
+		}
+
+		return () => document.removeEventListener("mousedown", handleClickOutside);
+	}, [isPopoverOpen]);
+
 	return (
-		<select
-			className={`${styles.select} ${className}`}
-			id="selectLang"
-			name="selectLang"
-			title={t("Button select language")}
-			value={lang}
-			onChange={e => languageChange(e)}>
-			{LANGUAGES.map(el => {
-				return (
-					<option key={el} value={el}>
-						{el.toUpperCase()}
-					</option>
-				);
-			})}
-		</select>
+		<div ref={popoverRef} className={clsx(styles.select, className)}>
+			<button className={styles.select__button} onClick={togglePopover}>
+				{lang.toUpperCase()}
+			</button>
+			{isPopoverOpen && (
+				<div className={styles.select__popover}>
+					{LANGUAGES.map(el => (
+						<button
+							key={el}
+							className={styles.select__popover__button}
+							onClick={() => languageChange(el)}>
+							{el.toUpperCase()}
+						</button>
+					))}
+				</div>
+			)}
+		</div>
 	);
 };
